5eb575ed0d356eba9f25b1cd9105e75addb473b3
Andrew Wittrock <andy@wittrock.us>
2023-03-13 16:19:05 -0500

    Add light/dark mode toggle

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,23 +1,36 @@
-import { FilesPanel } from '@/components/FilesPanel'
+import { FilesPanel } from '@/components/FilesPanel';
 import { Text2Image } from '@/components/Text2Image';
-import {ConfigProvider, theme, Layout} from 'antd';
+import { Col, ConfigProvider, Layout, Row, Switch, theme } from 'antd';
+import { useState } from 'react';
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
+  const [dark, setDark] = useState(true);
+  const toggleDarkMode = () => setDark(d => !d);
+
   return <ConfigProvider
     theme={{
-      algorithm: theme.darkAlgorithm,
+      algorithm: dark ? theme.darkAlgorithm : theme.defaultAlgorithm,
     }}
   >
     <Layout className={styles.app}>
-      <Layout.Header><b>Stable Diffusion Web GUI</b></Layout.Header>
+      <Layout.Header>
+        <b>Stable Diffusion Web GUI</b>
+        <div className={styles.modeSwitch}>
+          <Switch checkedChildren="Dark" unCheckedChildren="Light" checked={dark} onChange={toggleDarkMode} />
+        </div>
+        </Layout.Header>
       <Layout>
         <Layout.Content className={styles.content}>
-          <Text2Image />
+          <Row>
+            <Col xs={20}>
+              <Text2Image />
+            </Col>
+            <Col xs={4}>
+              <FilesPanel />
+            </Col>
+          </Row>
         </Layout.Content>
-        <Layout.Sider className={styles.rightSider} width={384}>
-          <FilesPanel />
-        </Layout.Sider>
       </Layout>
       <Layout.Footer>
         Footer goes here
